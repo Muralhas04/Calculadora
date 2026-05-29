ff850bc07f8cc3ee2cb6ab75a5b00f9c2c103880
Muralhas04 <luisfaiscacandeias@gmail.com>
2026-05-29 11:02:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def calculadora(num1: float, num2: float, operador: str) -> float:
     sinalizando que o cálculo não pôde ser realizado.
     """
 result = float("nan")
-    if operador == '+':
+if operador == '+':
         result = num1 + num2
     elif operador == '-':
         result = num1 - num2
@@ -68,7 +68,7 @@ try:
             print("\nOperações disponíveis: +, -, *, /, ** (potência), % (resto)")
             operador = input('Escolha o tipo de operação: ').strip()
         
-resultado = calculadora(num1, num2, operador)
+            resultado = calculadora(num1, num2, operador)
             
             if math.isnan(resultado):
                 print('\nOperação inválida! -> Tente novamente!')
