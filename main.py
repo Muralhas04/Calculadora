3e804acb1a5872dca26692b2d30c993507a8d70e
Muralhas04 <luisfaiscacandeias@gmail.com>
2026-05-29 10:38:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,12 @@ def calculadora_alternativa(num1: float, num2: float, operador: str) -> float:
                 return float("nan")
         case '**':
             return num1 ** num2
+        case '%':
+            if num2 != 0:
+                return num1 % num2
+            else:
+                print('Impossível dividir por zero!')
+                return float("nan")
         
 
 
