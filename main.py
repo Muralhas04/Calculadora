650dd55e63979c0c3eb035b8747bc6902691bcb3
Muralhas04 <luisfaiscacandeias@gmail.com>
2026-05-29 10:33:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def calculadora(num1: float, num2: float, operador: str) -> float:
         result = num1 + num2
     elif operador == '-':
         result = num1 - num2
-   
+    elif operador == '*':
             
     return result
 
